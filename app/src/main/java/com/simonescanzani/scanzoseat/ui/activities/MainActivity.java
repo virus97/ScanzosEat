06fdf21660f0f27828d54a3ed46d726b138bb7e1
simonescanzani <simonescanzani97@gmail.com>
2019-01-31 23:12:39 +0100

    Seventh Commit

--- app/src/main/java/com/simonescanzani/scanzoseat/ui/activities/MainActivity.java
+++ app/src/main/java/com/simonescanzani/scanzoseat/ui/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.simonescanzani.scanzoseat.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
 
         lstShop = new ArrayList<>();
         lstShop.add(new Shop("The Vegitarian","Categorie Shop","Description shop",R.drawable.thevigitarian));
